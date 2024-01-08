53693b263b8077729aefb09f02a56fe0c84a65ef
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-08 14:31:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	let pattern = /^([a-zA-Z0-9\.]+@+[a-zA-Z]+(\.)+[a-zA-Z]{2,3})$/;
+	var result = pattern.test(str);
+	return result;
 }
+
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
